354fc97839ec9cb77e011edccdbcf337370f9b0b
haz0 <73988148+haz0x676@users.noreply.github.com>
2023-02-04 14:13:34 +0300

    task 1

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import sys
 
 import requests
 from PyQt5 import uic
-from PyQt5.QtGui import QPixmap, QWheelEvent
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
 
 SCREEN_SIZE = [600, 450]
 
 
-class MainWindow(QMainWindow):
+class StaticMap(QMainWindow):
     current_LL = (37.530887, 55.703118)
     current_spn = (0.002, 0.002)
     current_map_type = 'map'
@@ -18,8 +18,8 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.initUI()
         self.getImage()
+        self.initUI()
 
     def getImage(self):
         map_params = {
@@ -34,36 +34,25 @@ class MainWindow(QMainWindow):
             print(self.map_api_server)
             print("Http статус:", response.status_code, "(", response.reason, ")")
 
+        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
+
+    def initUI(self):
+        uic.loadUi('ui\main_window.ui', self)  # Загружаем дизайн
+
+        ## Изображение
         self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
 
-    def initUI(self):
-        uic.loadUi('ui/main_window.ui', self)
-
-    def wheelEvent(self, event: QWheelEvent) -> None:
-        if event.angleDelta().y() > 0:
-            self.changeMapScale('-')
-        else:
-            self.changeMapScale('+')
-        self.getImage()
-
-    def changeMapScale(self, eventScaleType: str):
-        current_change = 1
-        if eventScaleType == '-':
-            current_change = -current_change
-        new_spn = (self.current_spn[0] + current_change, self.current_spn[1] + current_change)
-        if 0 <= new_spn[0] <= 90 and 0 <= new_spn[1] <= 90:
-            self.current_spn = new_spn
-
     def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainWindow()
+    ex = StaticMap()
     ex.show()
     sys.exit(app.exec())
